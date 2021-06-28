d32fda7d7a75dbcc5d94c45719f468960fa28209
Prabhanjan <Prabhanjan.official@gmail.com>
2021-06-28 10:11:40 +0530

    new code

--- movie-seat-booking/script.js
+++ movie-seat-booking/script.js
@@ -4,16 +4,25 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 let ticket_price = +movieSelect.value;
+let bookedSeats = [];
 
 function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
     console.log(selectedSeats);
     
-    const seatsIndex = [...selectedSeats].map(function(seat) {
-        return [...seats].indexOf(seat);
+    selectedSeats.forEach(el => {
+        if (bookedSeats.length > 0) {
+            bookedSeats.forEach(id => {
+                if (bookedSeats.indexOf(el.id) === -1) {
+                    bookedSeats.push(el.id);
+               }
+            });
+        } else {
+            bookedSeats.push(el.id);
+       }
     });
 
-    console.log(seatsIndex);
+    console.log(bookedSeats);
 
     const selectedSeatCount = selectedSeats.length;
     
@@ -29,7 +38,7 @@ movieSelect.addEventListener('change', (e) => {
 
 container.addEventListener('click', (e) => {
     if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
-        console.log(e.target);
+        // console.log(e.target);
     }{
         e.target.classList.toggle('selected');
 
